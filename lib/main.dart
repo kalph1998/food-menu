6b065e8828c157f181da250b5f47cf8251efa521
KalphU <kalph.U@Kalphs-MacbookPro.local>
2023-05-27 19:40:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:food_menu/constants.dart';
+import 'package:food_menu/screens/onBoard.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Food Menu',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+        primaryColor: kPrimaryColor,
+        useMaterial3: true,
+      ),
+      home: const OnboardScreen(),
+    );
+  }
+}
---
+++ lib/screens/onBoard.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:food_menu/constants.dart';
+
+class OnboardScreen extends StatelessWidget {
+  const OnboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: kPrimaryColor,
+    );
+  }
+}
