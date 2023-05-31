19623c534ca0e72b1b505620c75d9963c937bfc3
KalphU <kalph.U@Kalphs-MacbookPro.local>
2023-05-31 16:50:45 +0530

    added tab logic

--- lib/components/mealDetailTab.dart
+++ lib/components/mealDetailTab.dart
@@ -20,62 +20,65 @@ class _MealDetailTabState extends State<MealDetailTab> {
         color: kDarkColor,
         borderRadius: BorderRadius.circular(40),
       ),
-      child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-        InkWell(
-          onTap: () {
-            setState(() {
-              selectedTab = TabValues.instructions;
-            });
-          },
-          child: Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: selectedTab == TabValues.instructions
-                  ? kDarkGreyColor
-                  : Colors.transparent,
-            ),
-            child: Text(
-              "Instructions",
-              style: TextStyle(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() {
+                selectedTab = TabValues.instructions;
+              });
+            },
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
                 color: selectedTab == TabValues.instructions
-                    ? kPrimaryColor
-                    : kDarkGreyFontColor,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+                    ? kDarkGreyColor
+                    : Colors.transparent,
+              ),
+              child: Text(
+                "Instructions",
+                style: TextStyle(
+                  color: selectedTab == TabValues.instructions
+                      ? kPrimaryColor
+                      : kDarkGreyFontColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-        InkWell(
-          onTap: () {
-            setState(() {
-              selectedTab = TabValues.ingredients;
-            });
-          },
-          child: Container(
-            alignment: Alignment.center,
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: selectedTab == TabValues.ingredients
-                  ? kDarkGreyColor
-                  : Colors.transparent,
-            ),
-            child: Text(
-              "Ingredients",
-              style: TextStyle(
+          InkWell(
+            onTap: () {
+              setState(() {
+                selectedTab = TabValues.ingredients;
+              });
+            },
+            child: Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
                 color: selectedTab == TabValues.ingredients
-                    ? kPrimaryColor
-                    : kDarkGreyFontColor,
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+                    ? kDarkGreyColor
+                    : Colors.transparent,
+              ),
+              child: Text(
+                "Ingredients",
+                style: TextStyle(
+                  color: selectedTab == TabValues.ingredients
+                      ? kPrimaryColor
+                      : kDarkGreyFontColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-        ),
-      ]),
+        ],
+      ),
     );
   }
 }
