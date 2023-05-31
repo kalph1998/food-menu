206dc7507aa0a238cd5cde674bfe4661f7143552
KalphU <kalph.U@Kalphs-MacbookPro.local>
2023-05-31 11:10:22 +0530

    added icon and names

--- lib/components/mealInfo.dart
+++ lib/components/mealInfo.dart
@@ -19,6 +19,102 @@ class MealInfo extends StatelessWidget {
               fontWeight: FontWeight.w700,
               fontSize: 26,
             ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 14),
+                child: Column(
+                  children: [
+                    const Icon(
+                      Icons.schedule,
+                      color: kPrimaryColor,
+                      size: 24,
+                    ),
+                    Text(
+                      "40 minute",
+                      style: TextStyle(
+                          color: kLightFontColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "Cooking",
+                      style: TextStyle(
+                          color: kLightGreyFontColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 30,
+                width: 1,
+                color: kDarkGreyFontColor,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 14),
+                child: Column(
+                  children: [
+                    const Icon(
+                      Icons.star,
+                      color: kPrimaryColor,
+                      size: 24,
+                    ),
+                    Text(
+                      "40 minute",
+                      style: TextStyle(
+                          color: kLightFontColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "Cooking",
+                      style: TextStyle(
+                          color: kLightGreyFontColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 30,
+                width: 1,
+                color: kDarkGreyFontColor,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 14),
+                child: Column(
+                  children: [
+                    const Icon(
+                      Icons.local_fire_department,
+                      color: kPrimaryColor,
+                      size: 24,
+                    ),
+                    Text(
+                      "40 minute",
+                      style: TextStyle(
+                          color: kLightFontColor,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      "Cooking",
+                      style: TextStyle(
+                          color: kLightGreyFontColor,
+                          fontSize: 12,
+                          fontWeight: FontWeight.w600),
+                    )
+                  ],
+                ),
+              ),
+            ],
           )
         ],
       ),
